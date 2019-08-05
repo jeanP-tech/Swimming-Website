4d999c674fa83b0115f487b3639d9018ba932b3d
jeanP-tech <jeanP.tech@gmail.com>
2019-08-05 19:43:18 +0900

    model modified

--- swimapp/models.py
+++ swimapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    city_name = models.CharField(max_length = 20, default='DEFAULT VALUE')
+
+class Pool(models.Model):
+    city = models.ForeignKey(City, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 30)
+
+'''
+class Price(models.Model):
+    pool = models.OneToOneField(Pool, on_delete = models.CASCADE)
+    age = models.CharField(max_length = 20)
+    price = models.IntegerField()
+
+class Timetable(models.Model):
+    pool = models.OneToOneField(Pool, on_delete = models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+'''
