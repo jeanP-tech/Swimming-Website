2ecd1eaa5363187d196368b515ded8d2c9fcc830
jeanP-tech <jeanP.tech@gmail.com>
2019-08-17 21:38:59 +0900

    view modified

--- swimapp/urls.py
+++ swimapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('detail', views.save_date, name = 'detail_page')
+    path('pools/', views.index, name='index'),
+    path('pools/detail', views.detail, name = 'detail')
 ]
--- swimapp/views.py
+++ swimapp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .models import City, Pool, Price, Timetable, Userinfo
 from .forms import SaveForm
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 import datetime
 
 def index(request):
     return render(request, 'swimapp/index.html', {})
 
-def save_date(request):
-    form = SaveForm()
-    return render(request, 'swimapp/pool.html', {'form': form})
+def save():
+    user_datetime = datetime.datetime.now()
+    user_datetime.save()
+
+def detail(request):
+
+    #form = SaveForm()
+    #return render(request, 'swimapp/pool.html', {'form': form})
