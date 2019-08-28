61eb0bdbaa568dc2bdaa3c6f23d697de921f1fb3
jeanP-tech <jeanP.tech@gmail.com>
2019-08-29 01:24:47 +0900

    view complted

--- swimapp/views.py
+++ swimapp/views.py
@@ -3,13 +3,13 @@ from .models import City, Pool, Price, Timetable, Userinfo
 from .forms import CheckForm
 from django.http import HttpResponseRedirect
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 def getinfo(request):
     if request.method == 'POST':
         form = CheckForm(request.POST)
         if form.is_valid():
-            
+            '''
             obj = Userinfo()
             obj.user_city = form.cleaned_data['cities']
             obj.user_time = datetime.now()
@@ -19,17 +19,16 @@ def getinfo(request):
             '''
             user_city = form.cleaned_data['cities']
             user_time = datetime.now()
-            user_timeinterval = form.cleaned_data['times']
+            user_timeinterval = int(form.cleaned_data['times'])
+            day = datetime.now()
 
-            hour = int(datetime.datetime.now().hour)
-            min = int(datetime.datetime.now().min)
+            hour = datetime.now().hour + user_timeinterval
             if hour >= 24:
                 hour -= 24
-                day = int(datetime.datetime.now().day + timedelta(days = 1))
-                day.isoweekday()
-            else:
-            '''
-            return HttpResponseRedirect('swimapp/pool')
+                day = timedelta(days=1, hours=user_timeinterval) - timedelta(hours=24)
+            weekday = day.isoweekday()
+            pool_list = Timetable.objects.filter(day=weekday, start_time__gte=datetime.now(), end_time__lt=time(hour))
+            return render(request, 'swimapp/pool.html', {'pool_list': pool_list})
     else:
         form = CheckForm()
     return render(request, 'swimapp/index.html', {'form':form})
