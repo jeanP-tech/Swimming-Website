9f4c2c05f927ed480b20e23e4beac0d1ee774c30
jeanP-tech <jeanP.tech@gmail.com>
2019-11-22 23:39:27 -0500

    Add pg1 pool_in_city_list

--- swimapp/views.py
+++ swimapp/views.py
@@ -30,7 +30,8 @@ def getinfo(request):
     else:
         form = CheckForm()
         city_list = City.objects.all()
-        return render(request, 'swimapp/index.html', {'form':form, 'city_list': city_list})
+        pool_in_city_list = Pool.objects.all()
+        return render(request, 'swimapp/index.html', {'form':form, 'city_list': city_list, 'pool_in_city_list': pool_in_city_list})
 
 # def poolListView(request):
 #     return render(request, 'swimapp/pool.html', {})
