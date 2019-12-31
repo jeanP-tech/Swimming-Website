3c2efe83abb1a836bc0c063458c9c016d3ee6278
jeanP-tech <jeanP.tech@gmail.com>
2019-12-31 22:58:05 +0900

    Divide note.js file from pool.html

---
+++ static/swimapp/js/note.js
@@ -0,0 +1,15 @@
+var note_contents = document.querySelectorAll(".note_contents");
+var note = document.querySelectorAll(".note");
+
+for(let i=0; i<note.length; i++){
+    note[i].addEventListener("click", function() {
+      if(!note_contents[i].style.display || note_contents[i].style.display === "none"){
+        note[i].innerHTML = "접기";
+        note_contents[i].style.display = "block";
+      }
+      else{
+        note[i].innerHTML = "자세히";
+        note_contents[i].style.display = "none";
+      }
+    });
+}
